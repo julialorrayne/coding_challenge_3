9acea80030f19d9aada4623dfb838ab128bda48d
Julia <lorrayne@usf.edu>
2025-02-02 15:40:28 -0500

    Task 1 - Product Price anagement.

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+//Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system.
+let prices = [5,10,15,20,25]; // declared an array with 5 numerical values
+prices.push(30); //added new price to the array
+prices.shift();//Removed the first price from the array
+
+console.log('Prices', prices); //Logged the updated price list to the console
+
